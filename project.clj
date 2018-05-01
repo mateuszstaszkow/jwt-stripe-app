9a6b0d175e8b6cf107382725369613e37a25b50d
Mateusz Staszków <staszkom@ee.pw.edu.pl>
2018-05-01 17:37:28 +0200

    Http kit setup

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject jwt-stripe-app "1.0.0-SNAPSHOT"
   :description "JWT, Stripe API, Web App"
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+				[http-kit "2.2.0"]]
   :main jwt-stripe-app.core)
--- src/jwt_stripe_app/core.clj
+++ src/jwt_stripe_app/core.clj
@@ -1,5 +1,13 @@
 (ns jwt-stripe-app.core
+  (:use org.httpkit.server)
   (:gen-class :main true))
 
 (defn -main []
   (println "Hello World!"))
+  
+(defn app [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+   
+(run-server app {:port 8080})
