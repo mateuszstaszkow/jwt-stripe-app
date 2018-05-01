04c45dc1d39e9a37555d95c8ae489d326b34201f
Mateusz Staszków <staszkom@ee.pw.edu.pl>
2018-05-02 01:05:27 +0200

    Http Kit Client test

--- src/jwt_stripe_app/core.clj
+++ src/jwt_stripe_app/core.clj
@@ -3,6 +3,7 @@
         [compojure.handler :only [site]] ; form, query params decode; cookie; session, etc
         [compojure.core :only [defroutes GET POST DELETE ANY context]]
         org.httpkit.server)
+  (:require [org.httpkit.client :as http])
   (:gen-class :main true))
 
 (defn -main []
@@ -14,10 +15,20 @@
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "hello HTTP!"})
+   
+(defn client-test [req]
+  (let [{:keys [status headers body error] :as resp} @(http/get "http://localhost:8888/app")]
+	  (if error
+		(println "Failed, exception: " error)
+		(println "HTTP GET success: " body))
+		{:status  200
+		 :headers {"Content-Type" "text/html"}
+		 :body    (str "http client used, result: " body)}))
   
 (defroutes all-routes
   (GET "/" [] show-landing-page)
   (GET "/app" [] app)     ; websocket
+  (GET "/test" [] client-test)
 ;  (context "/user/:id" []
 ;           (GET / [] get-user-by-id)
 ;           (POST / [] update-userinfo))
