469defd137357fb94bee274437a8e9d6120e9d7f
Mateusz Staszków <staszkom@ee.pw.edu.pl>
2018-05-04 17:40:44 +0200

    Add integration tests

--- test/jwt_stripe_app/core_test.clj
+++ test/jwt_stripe_app/core_test.clj
@@ -7,22 +7,69 @@
 (defn build-login-body [] 
   (def login_body "{\"login\": \"merchant1\", \"password\": \"haslo\"}")
   (bytes (byte-array (map (comp byte int) login_body))))
+  
+(defn build-auth-headers []
+  {"cookie" ((:headers (login (build-login-body))) "Set-Cookie")})
 			
 (fact "get merchant id from JWT of merchant1 after login"
   ; Given
-  (def token ((:headers (login (build-login-body))) "Set-Cookie"))
-  ; When => Then
+  (def login_body (build-login-body))
+  ; When
+  (def token ((:headers (login login_body)) "Set-Cookie"))
+  ; Then
   (:merchant_id (get-credentials-from-token token)) => "merchant1")
   
+(fact "get not authorized status for plan endpoint"
+  ; Given
+  (def headers {"cookie" ""})
+  ; When
+  (def response (get-plan-auth headers))
+  ; Then
+  (:status response) => 401)
+  
+(fact "get not authorized status for allowed plans endpoint"
+  ; Given
+  (def headers {"cookie" ""})
+  ; When
+  (def response (get-allowed-plans-auth headers))
+  ; Then
+  (:status response) => 401)
+  
+(fact "get not authorized status for update plan endpoint"
+  ; Given
+  (def headers {"cookie" ""})
+  ; When
+  (def response (update-plan-auth nil headers))
+  ; Then
+  (:status response) => 401)
+  
 (fact "clear JWT cookie after logout"
-  ; When => Then
-  ((:headers (logout nil)) "Set-Cookie") => "")
+  ; When
+  (def token ((:headers (logout nil)) "Set-Cookie"))
+  ; Then
+  token => "")
   
-(fact "get subscription plan for username user"
-  )
+(fact "get subscription plan for user merchant1"
+  ; Given
+  (def headers (build-auth-headers))
+  ; When
+  (def response (get-plan-auth headers))
+  ; Then
+  (:status response) => 200)
   
-(fact "get allowed subscription plans for username user"
-  )
+(fact "get allowed subscription plans for user merchant1"
+  ; Given
+  (def headers (build-auth-headers))
+  ; When
+  (def response (get-allowed-plans-auth headers))
+  ; Then
+  (:status response) => 200)
   
-(fact "update subscription plan for username user"
-  )
+(fact "update subscription plan for user merchant1"
+  ; Given
+  (def headers (build-auth-headers))
+  (def update_dto_bytes (bytes (byte-array (map (comp byte int) "{}"))))
+  ; When
+  (def response (get-allowed-plans-auth headers))
+  ; Then
+  (:status response) => 200)
